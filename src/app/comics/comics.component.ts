e57009ad73ed241664ed3e767280bc8d053c7ea7
BorosFather <boros.zoli024@gmail.com>
2024-03-13 23:01:18 +0100

    kapcsolat fb, insta linkelve

--- src/app/comics/comics.component.ts
+++ src/app/comics/comics.component.ts
@@ -5,18 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './comics.component.html',
   styleUrls: ['./comics.component.scss']
 })
+
 export class ComicsComponent {
   comics!: { url: string; text: string }[][];
   
   ngOnInit(): void {
     
     const comics = [
-      { url: "../../assets/imgs/comic/spi1-cover.webp", text: "eyg"},
-      { url: "../../assets/imgs/comic/spi2-cover.webp", text: "ketto"},
-      { url: "../../assets/imgs/comic/spi3-cover.webp"},
-      { url: "../../assets/imgs/comic/spi4-cover.webp"},
-      { url: "../../assets/imgs/comic/spi5-cover.webp"},
-      { url: "../../assets/imgs/comic/spi6-cover.webp"},
+      { url: "../../assets/imgs/comic/spi1-cover.webp", text: "1.rész"},
+      { url: "../../assets/imgs/comic/spi2-cover.webp", text: "2.rész"},
+      { url: "../../assets/imgs/comic/spi3-cover.webp", text: "3.rész"},
+      { url: "../../assets/imgs/comic/spi4-cover.webp", text: "4.rész"},
+      { url: "../../assets/imgs/comic/spi5-cover.webp", text: "5.rész"},
+      { url: "../../assets/imgs/comic/spi6-cover.webp", text: "6.rész"},
     ];
 
     this.comics = this.chunkArray(comics, 3);
