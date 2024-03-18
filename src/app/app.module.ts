8ef025dbc7c3979bfc8e585751b348ec1f94c273
BorosFather <boros.zoli024@gmail.com>
2024-03-18 10:46:44 +0100

    detail component letrehozva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AboutComponent } from './about/about.component';
 import { ComicsComponent } from './comics/comics.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FooterComponent } from './footer/footer.component';
     AboutComponent,
     ComicsComponent,
     ContactComponent,
-    FooterComponent
+    FooterComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
